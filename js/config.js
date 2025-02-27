fc12d905608b1fb81a75f055c719237908b10267
maheenkhalid-coder <64140831+maheenkhalid-coder@users.noreply.github.com>
2025-02-28 00:33:30 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-central-1_5fvajtYGb', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: 'h6dc8qp4f77123g9vblbu786l', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5s4g7sbhq4pc7bflk9s55e7t9k', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
